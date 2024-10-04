35d82b8ca6c0b821079402407936388516555dfc
Maxim Baklanov <86889314+baklanomax@users.noreply.github.com>
2024-10-04 06:45:45 +0500

    Add files via upload

---
+++ files/linuxtune.sh
@@ -0,0 +1,5 @@
+PROMPT_COMMAND="history -a; history -n"
+/usr/sbin/kbdrate -r 2.0 -d 1000 > /dev/null
+alias mc="mc -u"
+alias mcedit="mcedit -u"
+setterm --background cyan --foreground yellow --bold on --store --clear=rest
